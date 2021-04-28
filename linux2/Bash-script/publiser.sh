a3dcb016c297c493f8884c8f39d32c0830b67727
Erik Storås Sommer <eriksommer99@gmail.com>
2021-04-28 22:32:59 +0200

    Forkortet til én for-test

--- linux2/Bash-script/publiser.sh
+++ linux2/Bash-script/publiser.sh
@@ -5,18 +5,10 @@ home=~
 if [ ! -d $home/www/bilder ]; then
    mkdir -p $home/www/bilder
 fi
-
 chmod 755 $home/www
 chmod 755 $home/www/bilder
 
-for file in $home/www
-do
-   if [ -f $file ]; then
-      chmod 644 $file
-   fi
-done
-
-for file in $home/www/bilder
+for file in $home/www/* $home/www/bilder/*
 do
    if [ -f $file ]; then
       chmod 644 $file
