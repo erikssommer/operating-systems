c40882e7b93fdae06c154025b04b6810ac3817b2
Erik Storås Sommer <eriksommer99@gmail.com>
2021-04-28 22:42:31 +0200

    Lagt til eksempel på bruk

--- linux2/Bash-script/rename.sh
+++ linux2/Bash-script/rename.sh
@@ -24,3 +24,8 @@ if [ ! $found ]
 then
     echo "Fant ingen filer med filendelse $fra"
 fi
+
+# Eksempel paa bruk
+# $ ./rename.sh wav mp3
+# Endrer fil.wav til fil.mp3
+# Endrer fil2.wav til fil2.mp3
