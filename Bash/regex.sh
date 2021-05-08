508040c4b9275b4c2c031f7b184d1f5619ca610f
Erik Storås Sommer <erikssommer@gmail.com>
2021-05-08 22:32:24 +0200

    La til en teller som avslutter etter 4 innlesninger

--- Bash/regex.sh
+++ Bash/regex.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 regexp="ho"
+teller=0
 
 echo "Matcher $regexp"
 
@@ -8,5 +9,12 @@ while read line
 do
   if [[ $line =~ $regexp ]]; then # Den vesentlige syntaksen er $line =~ $regexp hvor nøyaktig det som er i $regexp$ blir matchet, hverken mer eller mindre.
     echo "match!"
+  fi
+
+  ((teller++))
+
+  if [ "$teller" -eq 4 ]; then
+    echo "Avslutter!"
+    exit 1
   fi
 done
