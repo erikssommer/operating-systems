0743475364d9d7ffe6f1504b5ad836b2bab9f5ef
Erik Storås Sommer <erikssommer@gmail.com>
2021-05-20 16:55:48 +0200

    Script leser fra cli og oppretter mappe

--- Bash/makedir.sh
+++ Bash/makedir.sh
@@ -6,6 +6,17 @@ if [ ! -d $1 ]; then
    cd $1
    touch fil1.txt
    touch fil2.txt
+   echo "Opprette ny mappe? (y/n): "
+   read line
+   if [ $line = "y" ]; then
+      echo "Hva skal mappen hete?: "
+      read line
+      mkdir $line
+      cd $line
+      touch fil3.txt
+   else
+      echo "Den er grei. Avslutter!"
+   fi
 else
    echo "Mappen eksisterer allerede"
 fi
