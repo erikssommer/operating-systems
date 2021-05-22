8fc061c220cf7da6db54bc35451854ba59dceca7
Erik Storås Sommer <erikssommer@gmail.com>
2021-05-22 14:05:29 +0200

    Kommentert i koden

--- teori/v18/scripts/proc.sh
+++ teori/v18/scripts/proc.sh
@@ -9,9 +9,9 @@ fi
 
 for i in $(seq 1 30)
 do
-  idle1=$(grep cpu /proc/stat | head -n 1 | cut -d ' ' -f $nr)
+  idle1=$(grep cpu /proc/stat | head -n 1 | cut -d ' ' -f $nr) # kolonneskille gitt ved -d ' '
   sleep 1
-  idle2=$(grep cpu /proc/stat | head -n 1 | cut -d ' ' -f $nr)
+  idle2=$(grep cpu /proc/stat | head -n 1 | cut -d ' ' -f $nr) # klipper ut kolonne $1 gitt ved -f2
   ((idle = idle2 - idle1))
   echo $idle
 done
