5df268bc273407804fd50e7d6e27d9a829134c04
Erik Storås Sommer <erikssommer@gmail.com>
2021-05-30 19:30:03 +0200

    Sende output og tid til to ulike filer

--- Bash/regnram.c
+++ Bash/regnram.c
@@ -2,11 +2,13 @@
 
 int array[102400];
 
-void main(){
+int main(){
    int i,k;
    for(k=0;k<2000000;k++){
       for(i=0;i<1024;i++){
          array[i]=i;
       }
    }
+   printf("Ferdig");
 }
+
