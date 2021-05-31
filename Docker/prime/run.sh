a62cf28c0dfc75372d136bc4296a87ea0d07c026
Erik Storås Sommer <erikssommer@gmail.com>
2021-05-31 23:00:53 +0200

    Oppdatert til a kjore prime i bakgrunn

--- Docker/prime/run.sh
+++ Docker/prime/run.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
-(time ./a.out) > res.txt 2> tid.txt
+(time ./a.out &) > res1.txt 2> tid1.txt
+(time ./a.out &) > res2.txt 2> tid2.txt
+(time ./a.out &) > res3.txt 2> tid3.txt
+(time ./a.out &) > res4.txt 2> tid4.txt
 
 sleep 15
