094cd2f86bd9926abd3c38d70921cf090d92264e
Erik Storås Sommer <erikssommer@gmail.com>
2021-06-03 13:23:07 +0200

    Bruker en for-lokke fremfor flere like linjer

--- Docker/prime/run.sh
+++ Docker/prime/run.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-(time ./a.out &) > res1.txt 2> tid1.txt
-(time ./a.out &) > res2.txt 2> tid2.txt
-(time ./a.out &) > res3.txt 2> tid3.txt
-(time ./a.out &) > res4.txt 2> tid4.txt
+for i in {1..4}
+do
+    (time ./a.out) > res$i.txt 2> tid$i.txt &
+done
 
 sleep 15
